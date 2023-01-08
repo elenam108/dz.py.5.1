fc73d6ef3b35526e833e3229d94aa195a01c9396
ElenaM <elenam108@yandex.ru>
2023-01-08 20:56:19 +0300

    first commit

---
+++ zadacha5.3.py
@@ -0,0 +1,21 @@
+#Реализуйте RLE алгоритм: реализуйте модуль сжатия и 
+#восстановления данных.
+
+def encode(s):
+ 
+    encoding = "" 
+    i = 0
+    while i < len(s):
+        count = 1
+        while i + 1 < len(s) and s[i] == s[i + 1]:
+            count = count + 1
+            i = i + 1
+        encoding += str(count) + s[i]
+        i = i + 1
+    return encoding
+ 
+if __name__ == '__main__':
+ 
+    s = 'AAAAANNNNNWWWWWWWWWW'
+    print(s)
+    print(encode(s))
